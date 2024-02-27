786df1eea70b55f69d72d2926691d238f2a98710
Riyaki15 <rizkyrakib@gmail.com>
2024-02-27 19:01:42 +0700

    update

--- cypress/support/LoginLocator.js
+++ cypress/support/LoginLocator.js
@@ -3,7 +3,6 @@ class LoginClass {
     user = "[data-test=username]"
     password = "[data-test=password]"
     loginBtn = "#login-button"
-    loginBtn = "#login-button"
 
 }
 export default new LoginClass()
